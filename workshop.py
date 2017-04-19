5f54123b4d06e0990b5a7b5b8f8d721dd3de3355
Rayan958 <rayandinar95800@gmail.com>
2017-04-19 09:59:11 +0200

    Creation df basic

--- workshop.py
+++ workshop.py
@@ -11,15 +11,24 @@ review = sqlContext.read.json("../yelp_dataset/yelp_academic_dataset_review.json
 user = sqlContext.read.json("../yelp_dataset/yelp_academic_dataset_user.json")
 
 business.cache()
+review.cache()
+user.cache()
 
+'''
 business.show()
 review.show()
 user.show()
+'''
 
 business.registerTempTable("business")
 review.registerTempTable("review")
 user.registerTempTable("user")
 
+business_basic = sqlContext.sql("SELECT business_id, stars FROM business")
+review_basic = sqlContext.sql("SELECT review_id,user_id, business_id, stars, date FROM review")
+user_basic = sqlContext.sql("SELECT user_id, average_stars FROM user")
+
+business_basic.show()
 
 sc.stop()
 
