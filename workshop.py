8cfe72e917f5459e947098038c5597268b192c69
Rayan958 <rayandinar95800@gmail.com>
2017-04-19 09:32:26 +0200

    New version creation df

--- workshop.py
+++ workshop.py
@@ -1,31 +1,16 @@
-from pyspark.sql import SparkSession,SQLContext,Row
 from pyspark import SparkContext
-from pyspark.sql.functions import *
+from pyspark.sql import SQLContext
 
-spark = SparkSession.builder.getOrCreate()
-sc = SparkContext(appName="Workshop")
-sqlContext=SQLContext(sc)
 
-#Read Json File : business, user & review
-business = spark.read.json("yelp_academic_dataset_business.json")
-user = spark.read.json("yelp_academic_dataset_user.json")
-review = spark.read.json("yelp_academic_dataset_review.json")
+#Creation of the spark context
+sc = SparkContext(appName="Big Data Workshop")
+sqlContext = SQLContext(sc)
 
-#GraphFrames
+user = sqlContext.read.json("yelp_academic_dataset_tip.json")
 
-#Create Dataframe
+user.show()
 
-table_business = business.map(lambda p: Row(id = p[0],business_stars = p[1]))
-df_business = sqlContext.createDataFrame(table_business)
+user.registerTempTable("user")
 
-table_user = user.map(lambda p: Row(id_user = p[0],friends = p[]))
-df_user = sqlContext.createDataFrame(table_user)
-
-table_review = review.map(lambda p: Row(review_id = p[0],user_id = p[],business_id= p[],stars = p[]))
-df_review = sqlContext.createDataFrame(table_review)
-
-#Put dataframe into table
-
-#friends_link = sqlContext.registerDataFrameAsTable(dataframe,"friends_link")
-#id_user = sqlContext.sql("SELECT source AS id from friends_link")
+sc.stop()
 
